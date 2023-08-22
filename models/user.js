c7316adaf528bcd3f5c60272ac5c4ee7784415ce
joshuadavis1990 <joshuadavis1990@me.com>
2023-08-22 10:32:40 +1000

    Add models folder; complete basic iteration of user model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  name: {
+    first: String,
+    last: String
+  },
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+})
+
+const UserModel = mongoose.model('User', userSchema)
+
+export default UserModel
