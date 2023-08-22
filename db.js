07c0a5b0d84af69705ca88c653f5709db72c4f3e
joshuadavis1990 <joshuadavis1990@me.com>
2023-08-22 13:01:27 +1000

    Added DB connection string

--- db.js
+++ db.js
@@ -2,3 +2,7 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 
 dotenv.config()
+
+mongoose.connect(process.env.ATLAS_DB_URL)
+  .then(m => console.log(m.connection.readyState == 1 ? 'Mongoose connected!' : 'Mongoose failed to connect.'))
+  .catch(err => console.log(err))
