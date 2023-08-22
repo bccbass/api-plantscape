be9d4d78b1251da81d0639c45b1e2dad37c144a2
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-08-22 12:00:56 +1000

    moved user routes, added jwt token install

---
+++ controller/spaceRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+// IMPORT NEEDED MODELS
+// import { Space } from ''
+// import { Area } from ''
+
+// Create a new instance of Router object:
+const router = Router()
+
