deab9d739c04db80241d79b47479f9031e9840f3
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-08-23 11:10:14 +1000

    updated seeds script, plantModel and added seeds script to json

--- seed.js
+++ seed.js
@@ -4,6 +4,15 @@ import { AreaModel } from './models/AreaModel.js'
 import { PlantModel } from './models/PlantModel.js'
 import { dbClose } from './db.js'
 
+// Big question is what is in the 'plants' array? List of ids from the API or our own plant madel? 
+// potential plant schema:
+// const plantSchema = new mongoose.Schema({
+//   APIreference: plant api Id,
+//   notes: a way to keep notes for each plant in reference to their areas.
+//   areas: [if standalone, plants could have array of areas in which theyre located. Otherwise we nest a plant object in a plants array in area]
+// })
+
+
 const usersSeed = [
   {
     name: {
@@ -11,7 +20,99 @@ const usersSeed = [
       last: "Campbell"
     },
     email: "ben@gmail.com",
-    password: "ben123"
+    password: "ben123",
+    spaces: [  {
+        name: "Front Yard",
+        isOutdoor: true,
+        isIndoor: false,
+        notes: "The front of my house is east facing. It receives strong sunlight in the morning.",
+        imgUrl: "",
+        areas: [  {
+            name: "Verandah",
+            notes: "",
+            // "AN ARRAY OF PLANTS FROM OUR OWN PLANT SCHEMA THAT HAS THE API ID REFERENCE AND OPTIONAL USER NOTES"
+            plants: []
+          },
+          {
+            name: "Letterbox Garden",
+            notes: ""
+          },
+          {
+            name: "Balcony",
+            notes: "",
+            plants: []
+          },
+          {
+            name: "Boundary Hedge",
+            notes: "",
+            plants: []
+          },
+          {
+            name: "Vegetable Garden",
+            notes: ""
+          }]
+      },
+      {
+        name: "Back Yard",
+        isOutdoor: true,
+        isIndoor: false,
+        notes: "The rear of my house is west facing. It receives strong sunlight in the afternoon.",
+        imgUrl: "",
+        areas: [  {
+            name: "Verandah",
+            notes: "",
+            plants: []
+          },
+          {
+            name: "Letterbox Garden",
+            notes: ""
+          },
+          {
+            name: "Balcony",
+            notes: "",
+            plants: []
+          },
+          {
+            name: "Boundary Hedge",
+            notes: "",
+            plants: []
+          },
+          {
+            name: "Vegetable Garden",
+            notes: ""
+          }]
+      },
+      {
+        name: "Indoors",
+        isOutdoor: false,
+        isIndoor: true,
+        notes: "Keep an eye out for that pesky fiddle leaf!",
+        imgUrl: "",
+        areas: [  {
+            name: "Verandah",
+            notes: "",
+            plants: []
+          },
+          {
+            name: "Letterbox Garden",
+            notes: ""
+          },
+          {
+            name: "Balcony",
+            notes: "",
+            plants: []
+          },
+          {
+            name: "Boundary Hedge",
+            notes: "",
+            plants: []
+          },
+          {
+            name: "Vegetable Garden",
+            notes: ""
+          }]
+      }],
+    allPlants: []
   },
   {
     name: {
@@ -19,7 +120,98 @@ const usersSeed = [
       last: "Davis"
     },
     email: "josh@gmail.com",
-    password: "josh123"
+    password: "josh123",
+    spaces: [{
+      name: "Front Yard",
+      isOutdoor: true,
+      isIndoor: false,
+      notes: "The front of my house is east facing. It receives strong sunlight in the morning.",
+      imgUrl: "",
+      areas: [  {
+          name: "Verandah",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Letterbox Garden",
+          notes: ""
+        },
+        {
+          name: "Balcony",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Boundary Hedge",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Vegetable Garden",
+          notes: ""
+        }]
+    },
+    {
+      name: "Back Yard",
+      isOutdoor: true,
+      isIndoor: false,
+      notes: "The rear of my house is west facing. It receives strong sunlight in the afternoon.",
+      imgUrl: "",
+      areas: [  {
+          name: "Verandah",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Letterbox Garden",
+          notes: ""
+        },
+        {
+          name: "Balcony",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Boundary Hedge",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Vegetable Garden",
+          notes: ""
+        }]
+    },
+    {
+      name: "Indoors",
+      isOutdoor: false,
+      isIndoor: true,
+      notes: "Keep an eye out for that pesky fiddle leaf!",
+      imgUrl: "",
+      areas: [  {
+          name: "Verandah",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Letterbox Garden",
+          notes: ""
+        },
+        {
+          name: "Balcony",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Boundary Hedge",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Vegetable Garden",
+          notes: ""
+        }]
+    }],
+    allPlants: []
   },
   {
     name: {
@@ -27,7 +219,101 @@ const usersSeed = [
       last: "Fung"
     },
     email: "kal@gmail.com",
-    password: "kal123"
+    password: "kal123",
+    spaces: [{
+      name: "Front Yard",
+      isOutdoor: true,
+      isIndoor: false,
+      notes: "The front of my house is east facing. It receives strong sunlight in the morning.",
+      imgUrl: "",
+      areas: [  {
+          name: "Verandah",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Letterbox Garden",
+          notes: ""
+        },
+        {
+          name: "Balcony",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Boundary Hedge",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Vegetable Garden",
+          notes: "",
+          plants: []
+        }]
+    },
+    {
+      name: "Back Yard",
+      isOutdoor: true,
+      isIndoor: false,
+      notes: "The rear of my house is west facing. It receives strong sunlight in the afternoon.",
+      imgUrl: "",
+      areas: [  {
+          name: "Verandah",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Letterbox Garden",
+          notes: ""
+        },
+        {
+          name: "Balcony",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Boundary Hedge",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Vegetable Garden",
+          notes: "",
+          plants: []
+        }]
+    },
+    {
+      name: "Indoors",
+      isOutdoor: false,
+      isIndoor: true,
+      notes: "Keep an eye out for that pesky fiddle leaf!",
+      imgUrl: "",
+      areas: [  {
+          name: "Verandah",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Letterbox Garden",
+          notes: ""
+        },
+        {
+          name: "Balcony",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Boundary Hedge",
+          notes: "",
+          plants: []
+        },
+        {
+          name: "Vegetable Garden",
+          notes: "",
+          plants: []
+        }]
+    }],
+    allPlants: []
   }
 ]
 
@@ -36,78 +322,8 @@ console.log("Deleted users")
 const users = await UserModel.insertMany(usersSeed)
 console.log("Inserted users")
 
-const spacesSeed = [
-  {
-    name: "Front Yard",
-    isOutdoor: true,
-    isIndoor: false,
-    notes: "The front of my house is east facing. It receives strong sunlight in the morning.",
-    imgUrl: ""
-  },
-  {
-    name: "Back Yard",
-    isOutdoor: true,
-    isIndoor: false,
-    notes: "The rear of my house is west facing. It receives strong sunlight in the afternoon.",
-    imgUrl: ""
-  },
-  {
-    name: "Indoors",
-    isOutdoor: false,
-    isIndoor: true,
-    notes: "Keep an eye out for that pesky fiddle leaf!",
-    imgUrl: ""
-  }
-]
 
-await SpaceModel.deleteMany()
-console.log("Deleted spaces")
-const spaces = await SpaceModel.insertMany(spacesSeed)
-console.log("Inserted spaces")
 
-const areasSeed = [
-  {
-    name: "Verandah",
-    notes: ""
-  },
-  {
-    name: "Letterbox Garden",
-    notes: ""
-  },
-  {
-    name: "Balcony",
-    notes: ""
-  },
-  {
-    name: "Boundary Hedge",
-    notes: ""
-  },
-  {
-    name: "Vegetable Garden",
-    notes: ""
-  }
-]
 
-await AreaModel.deleteMany()
-console.log("Deleted areas")
-const areas = await AreaModel.insertMany(areasSeed)
-console.log("Inserted areas")
-
-const plantsSeed = [
-  {
-    name: {
-      common: "European Silver Fir",
-      scientific: "Abies alba"
-    },
-    description: "Amazing garden plant that is sure to capture attention...",
-    light: "Partial shade",
-    care: "Medium"
-  }
-]
-
-await PlantModel.deleteMany()
-console.log("Deleted plants")
-const plants = await PlantModel.insertMany(plantsSeed)
-console.log("Inserted plants")
 
 dbClose()
