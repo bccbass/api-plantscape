f697615fa3ad8c4136a4813645e9a0b81381ec15
joshuadavis1990 <joshuadavis1990@me.com>
2023-08-28 20:37:09 +1000

    Added extra tests to userRoutes.test.js

--- controllers/userRoutes.test.js
+++ controllers/userRoutes.test.js
@@ -1,6 +1,31 @@
 import app from "../app.js"
 import request from "supertest"
 
+describe("GET /users", () => {
+  let res
+
+  beforeEach(async () => {
+    res = await request(app).get("/users")
+  })
+
+  test("Returns JSON", () => {
+    expect(res.status).toBe(200)
+    expect(res.header["content-type"]).toMatch("json")
+  })
+
+  test("Returns an array of 3 elements", () => {
+    expect(res.body).toBeInstanceOf(Array)
+    expect(res.body).toHaveLength(3)
+  })
+
+  test("Each user has a valid 'firstName' and '_id'", () => {
+    res.body.forEach((el) => {
+      expect(el._id).toBeDefined()
+      expect(el.firstName).toBeDefined()
+    })
+  })
+})
+
 describe("POST /users/register", () => {
   let res
 
@@ -20,39 +45,22 @@ describe("POST /users/register", () => {
     expect(res.body._id).toBeDefined()
   })
 
-  // test("firstName has _id and correct name", () => {
-  //   expect(res.body.firstName).toBeDefined()
-  //   expect(res.body.firstName._id).toBeDefined()
-  //   expect(res.body.firstName.name).toBeDefined()
-  //   expect(res.body.firstName.name).toBe("Test")
-  // })
+  test("firstName has the correct value", () => {
+    expect(res.body.firstName).toBeDefined()
+    expect(res.body.firstName).toBe("Elton")
+  })
+
+  test("lastName has the correct value", () => {
+    expect(res.body.lastName).toBeDefined()
+    expect(res.body.lastName).toBe("John")
+  })
+
+  test("email has the correct value", () => {
+    expect(res.body.email).toBeDefined()
+    expect(res.body.email).toBe("ejohn@gmail.com")
+  })
 
   afterAll(async () => {
     await request(app).delete(`/users/${res.body._id}`)
-  })
-})
-
-describe("GET /users", () => {
-  let res
-
-  beforeEach(async () => {
-    res = await request(app).get("/users")
-  })
-
-  test("Returns JSON", () => {
-    expect(res.status).toBe(200)
-    expect(res.header["content-type"]).toMatch("json")
-  })
-
-  test("Returns an array of 3 elements", () => {
-    expect(res.body).toBeInstanceOf(Array)
-    // expect(res.body).toHaveLength(13)
-  })
-
-  test("Each category has a valid 'firstName' and '_id'", () => {
-    res.body.forEach((el) => {
-      expect(el._id).toBeDefined()
-      expect(el.firstName).toBeDefined()
-    })
   })
 })
