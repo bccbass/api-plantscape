15b73b6392e8dbd9d5b7894a132700ade415e064
joshuadavis1990 <joshuadavis1990@me.com>
2023-08-28 11:23:10 +1000

    Started on /users test

--- app.test.js
+++ app.test.js
@@ -7,6 +7,15 @@ describe("App Test", () => {
   test("GET /", async () => {
     // Simulate a get request to the app's "/" path and return intercepted response object
     const res = await request(app).get("/")
+    // Test conditions
+    expect(res.status).toBe(200)
+    expect(res.header['content-type']).toMatch('application/json')
+    expect(res.body.info).toBeDefined()
+    expect(res.body.info).toBe("Plantscape Index")
+  })
+
+  test("GET /users", async () => {
+    const res = await request(app).get("/users")
     expect(res.status).toBe(200)
     expect(res.header['content-type']).toMatch('application/json')
     expect(res.body.info).toBeDefined()
