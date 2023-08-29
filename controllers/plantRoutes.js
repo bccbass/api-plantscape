5ca133209a3df4d5eb111fe94f9ac54b7cd175f9
joshuadavis1990 <joshuadavis1990@me.com>
2023-08-29 11:54:02 +1000

    Added GET /plants test suite

--- controllers/plantRoutes.js
+++ controllers/plantRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import { authenticated } from "./userFunctions.js"
 
 // IMPORT NEEDED MODELS
 
@@ -15,9 +16,26 @@ const getPlantData = async (plantId ) => {
           return results
         }
 
-router.get("/", async (req, res) => {
-  const query = req.query.q;
+// POST ROUTE TO PLANTS TO FETCH LIST OF PLANT IDS FROM USER.PLANTS
+router.post("/", authenticated, async (req, res) => {
   const plantIds = req.body;
+  try {
+    if (plantIds.length) {
+      const plantPromiseList = []
+    plantIds.map(async (plantId) => {
+          plantPromiseList.push(getPlantData(plantId))
+        })
+        Promise.all(plantPromiseList).then(plantList => res.status(200).json(plantList))
+    }
+    else (res.status(200).json({Error: "No queries supplied"}))
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+// GET ROUTE TO PLANTS TO HANDLE USER QUERIES FOR PLANTS
+router.get("/", authenticated, async (req, res) => {
+  const query = req.query.q;
   try {
     if (query) {
       await fetch(
@@ -25,13 +43,7 @@ router.get("/", async (req, res) => {
       )
         .then((data) => data.json())
         .then((results) => res.status(200).json(results.data));
-    } else if (plantIds.length) {
-        const plantPromiseList = []
-      plantIds.map(async (plantId) => {
-            plantPromiseList.push(getPlantData(plantId))
-          })
-          Promise.all(plantPromiseList).then(plantList => res.status(200).json(plantList))
-      } else (res.status(200).json({Error: "No queries supplied"}))
+    }  else (res.status(200).json({Error: "No queries supplied"}))
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
--- controllers/plantRoutes.test.js
+++ controllers/plantRoutes.test.js
@@ -6,29 +6,47 @@ import request from "supertest"
 // GET /PLANTS TEST SUITE
 
 describe("GET /plants", () => {
+  let user
+  let token
+  let login
   let res
 
-  beforeEach(async () => {
-    res = await request(app).get("/plants").query({ q: 'monstera' })
-    console.log(res)
+  beforeAll(async () => {
+    login = await request(app).post("/users/login").send({
+      email: "josh@gmail.com",
+      password: "josh123"
+    })
+    token = login._body.token
+  })
+
+  beforeAll(async () => {
+    user = await request(app).get("/plants").set('Authorization', `Bearer ${token}`).query({ q: 'monstera' })
   })
 
   test("Returns JSON", () => {
-    expect(res.status).toBe(200)
-    expect(res.header["content-type"]).toMatch("json")
+    expect(user.status).toBe(200)
+    expect(user.header["content-type"]).toMatch("json")
   })
 
   test("Returns an array", () => {
-    expect(res.body).toBeInstanceOf(Array)
+    expect(user.body).toBeInstanceOf(Array)
   })
 
   test("Searches API for correct plants and returns them in an array of 2 plants", () => {
-    expect(res.body).toHaveLength(2)
-    expect(res.body[0].common_name).toBe("Swiss cheese plant")
+    expect(user.body).toHaveLength(2)
+    expect(user.body[0].common_name).toBe("Swiss cheese plant")
   })
 
   test("API fetch includes the plant's common and scientific name", () => {
-    expect(res.body[0]).toHaveProperty("common_name")
-    expect(res.body[0]).toHaveProperty("scientific_name")
+    expect(user.body[0]).toHaveProperty("common_name")
+    expect(user.body[0]).toHaveProperty("scientific_name")
+  })
+
+  test("String query (q) must be supplied", async () => {
+    res = await request(app).get("/plants").set('Authorization', `Bearer ${token}`)
+    console.log(res)
+    expect(res.status).toBe(200)
+    expect(res.header["content-type"]).toMatch("json")
+    expect(res.text).toContain("Error")
   })
 })
