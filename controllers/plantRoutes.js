c1d7a6f4600b6083207c416da22d7dad56cfddf6
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-08-31 14:56:02 +1000

    added try/catch to get plant by id route

--- controllers/plantRoutes.js
+++ controllers/plantRoutes.js
@@ -1,35 +1,42 @@
 import { Router } from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import { authenticated } from "./userFunctions.js"
+import { authenticated } from "./userFunctions.js";
 
 // IMPORT NEEDED MODELS
 
 // Create a new instance of Router object:
 const router = Router();
 
-const getPlantData = async (plantId ) => {
-  console.log('from get plant data')
-  console.log(`https://perenual.com/api/species/details/${plantId}?key=${process.env.PERENUAL_KEY}`)
-    const data =await fetch(
-        `https://perenual.com/api/species/details/${plantId}?key=${process.env.PERENUAL_KEY}`
-      )
-        const results = await data.json()
-          return results
-        }
+const getPlantData = async (plantId) => {
+  console.log("from get plant data");
+  console.log(
+    `https://perenual.com/api/species/details/${plantId}?key=${process.env.PERENUAL_KEY}`
+  );
+  try {
+    const data = await fetch(
+      `https://perenual.com/api/species/details/${plantId}?key=${process.env.PERENUAL_KEY}`
+    );
+    const results = await data.json();
+    return results;
+  } catch (e) {
+    res.status(400).send({ Error: e.message });
+  }
+};
 
 // POST ROUTE TO PLANTS TO FETCH LIST OF PLANT IDS FROM USER.PLANTS
 router.post("/", authenticated, async (req, res) => {
   const plantIds = req.body;
   try {
     if (plantIds.length) {
-      const plantPromiseList = []
-    plantIds.map(async (plantId) => {
-          plantPromiseList.push(getPlantData(plantId))
-        })
-        Promise.all(plantPromiseList).then(plantList => res.status(200).json(plantList))
-    }
-    else (res.status(200).json({Error: "No queries supplied"}))
+      const plantPromiseList = [];
+      plantIds.map(async (plantId) => {
+        plantPromiseList.push(getPlantData(plantId));
+      });
+      Promise.all(plantPromiseList).then((plantList) =>
+        res.status(200).json(plantList)
+      );
+    } else res.status(200).json({ Error: "No queries supplied" });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
@@ -45,7 +52,7 @@ router.get("/", authenticated, async (req, res) => {
       )
         .then((data) => data.json())
         .then((results) => res.status(200).json(results.data));
-    }  else (res.status(200).json({Error: "No queries supplied"}))
+    } else res.status(200).json({ Error: "No queries supplied" });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
